2dfb333019db22182103c34dba4a31a359b5b2f2
Eddie Tang <eddie@valtix.com>
2020-10-28 23:26:39 +0000

    added network tag outputs

--- outputs.tf
+++ outputs.tf
@@ -23,5 +23,17 @@ output "egress_subnet_id" {
 }
 
 output "instance_ip" {
-  value = google_compute_instance.vm_instance.network_interface.0.access_config.0.nat_ip 
+  value = google_compute_instance.vm_instance.network_interface.0.access_config.0.nat_ip
+}
+
+output "mgmt_network_tag" {
+  value = module.vpc.mgmt_network_tag
+}
+
+output "datapath_network_tag" {
+  value = module.vpc.datapath_network_tag
+}
+
+output "egress_network_tag" {
+  value = module.vpc.egress_network_tag
 }
